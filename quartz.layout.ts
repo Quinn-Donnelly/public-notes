30db51337ca1d1acd2e5331a9bb450c4a6db66c0
Quinn Donnelly <quinndonnelly22@gmail.com>
2024-09-18 20:03:48 -0400

    feat: changing around scaling for graph view

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,8 +38,8 @@ export const defaultContentPageLayout: PageLayout = {
                 scale: 0.8, // default view scale
                 repelForce: 1,  // how much nodes should repel each other
                 centerForce: 0.3, // how much force to use when trying to center the nodes
-                linkDistance: 30, // how long should the links be by default?
-                fontSize: 0.5, // what size should the node labels be?
+                linkDistance: 70, // how long should the links be by default?
+                fontSize: 0.6, // what size should the node labels be?
                 opacityScale: 100, // how quickly do we fade out the labels when zooming out?
                 removeTags: [], // what tags to remove from the graph
                 showTags: false, // whether to show tags in the graph
@@ -51,7 +51,7 @@ export const defaultContentPageLayout: PageLayout = {
                 scale: 0.9,
                 repelForce: 0.5,
                 centerForce: 0.3,
-                linkDistance: 30,
+                linkDistance: 100,
                 fontSize: 0.6,
                 opacityScale: 1,
                 removeTags: [], // what tags to remove from the graph
